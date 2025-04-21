87c53c14f1f7fb682104e7f25fc0f45f15217a28
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-04-22 00:03:07 +0300

    feat: render menu items from data.js and public/images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+import Title from './Title.jsx';
+import Menu from './Menu.jsx';
+
+import menu from './data.js';
+
 function App() {
-	return <h1>Menu</h1>;
+	const [menuItems, setMenuItems] = useState(menu);
+
+	return (
+		<main>
+			<section className="menu">
+				<Title text="Our menu" />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	);
 }
 
 export default App;
