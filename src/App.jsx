89bd7b1d770f10620d63ed538e063b0559b2000c
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-04-23 21:16:29 +0300

    feat: render all unique categories and display it

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import Title from './Title.jsx';
 import Menu from './Menu.jsx';
+import Categories from './Categories.jsx';
 
 import menu from './data.js';
 
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
+
 function App() {
 	const [menuItems, setMenuItems] = useState(menu);
+	const [categories, setCategories] = useState(allCategories);
 
 	return (
 		<main>
 			<section className="menu">
 				<Title text="Our menu" />
+				<Categories categories={categories} />
 				<Menu items={menuItems} />
 			</section>
 		</main>
